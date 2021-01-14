6832e6bc2e1a8d6529ebfc380999fd4e81cc621e
alaabbas97@gmail.com <alaabbas97@gmail.com>
2021-01-14 13:59:12 +0200

    change 2

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export DATABASE_URL='postgresql://postgres:Galaxy@0426@localhost:5432/casting_agency'
+export AUTH0_CLIENT_ID = 'vL7PimaOlhKGV9g1KjyyClHhim379Bnd\'
+export AUTH0_CLIENT_SECRET = '3oztwq3VLfAW5lAKhtd9qnMOIiDPfeBJ42XzA9y7ae8w8V1E00exKdRVxx7recuc\'
+export AUTH0_DOMAIN = 'fsndalaa.us.auth0.com'
+export API_AUDIENCE= 'fsndalaa.us.auth0.com'
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
