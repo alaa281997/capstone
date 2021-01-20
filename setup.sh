54c41cba3296acefa8ba3ce734f910d9029e9500
alaabbas97@gmail.com <alaabbas97@gmail.com>
2021-01-20 16:25:11 +0200

    new changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export DATABASE_URL='postgresql://postgres:alaa1997@localhost:5432/agency'
-export AUTH0_CLIENT = 'vL7PimaOlhKGV9g1KjyyClHhim379Bnd'
-export AUTH0_CLIENT_SECRET = '3oztwq3VLfAW5lAKhtd9qnMOIiDPfeBJ42XzA9y7ae8w8V1E00exKdRVxx7recuc\'
+export AUTH0_CLIENT = 'P52KazXl51gyf1r1L0bPUlBV6GcS53Hy'
+export AUTH0_CLIENT_SECRET = '_p0HARCWRpbTb3U-prS9dPFcPgqaBa4qEeG1V4TRIW2fBKsCtv49EfNGo5aeu7iu'
 export AUTH0_DOMAIN = 'fsndalaa.us.auth0.com'
 export API_AUDIENCE= 'fsndalaa.us.auth0.com'
 export ENV='development'
